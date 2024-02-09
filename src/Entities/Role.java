424515d6fb125dc1168aee01d6aa99bcb0067f81
henriquebenjamim <henriiqqq@gmail.com>
2024-02-08 23:06:43 -0300

    Initial commit with Role entity added

---
+++ src/Entities/Role.java
@@ -0,0 +1,32 @@
+package Entities;
+
+public class Role {
+
+    public String name;
+    public Double salary;
+    public Double benefit;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getBenefit() {
+        return benefit;
+    }
+
+    public void setBenefit(Double benefit) {
+        this.benefit = benefit;
+    }
+}
